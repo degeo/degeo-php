0428dcb0cd6d5dc6deb3442f2d25fb06baf30572
Jay Fortner <w3prodigy@gmail.com>
2019-10-22 20:40:13 -0400

    template for Crawler

---
+++ libraries/Crawler.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * DeGeo\Crawler
+ *
+ * @package DeGeo-PHP
+ * @since 0.0.1
+ * @version 0.0.1
+ */
+namespace DeGeo;
+/**
+ * Crawler
+ *
+ * Reads the contents of a web page
+ * Identities additional links on the web page
+ *
+ * @author Jay Fortner <jay@degeo.net>
+ */
+class Crawler {
+
+
+
+} // class
